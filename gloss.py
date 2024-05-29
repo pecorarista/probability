732097e77559c8210f4f4ce7cdf0d9b22906f9d4
Miyazawa Akira <pecorarista@gmail.com>
2024-05-30 00:32:01 +0900

    Update

---
+++ gloss.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import queue
+
+DELIM = '\t'
+
+def main() -> None:
+    q = queue.Queue()
+    with (
+        Path('laplace.csv').open(mode='r') as r,
+        Path('gloss.tex').open(mode='w') as w
+    ):
+        for (i, line) in enumerate(r):
+            if len(line.strip()) == 0:
+                (fr, pr, en, trans) = zip(*q.queue)
+                s = f'''
+    \\trigloss{{{' '.join(fr)}}}
+              {{{' '.join(pr)}}}
+              {{{' '.join(en)}}}
+              {{{' '.join(trans)}}}\n'''
+                w.write(s)
+                q = queue.Queue()
+            else:
+                try:
+                    (fr, pr, en, trans) = line.replace('\n', '').split(DELIM)
+                    q.put((fr, pr, en, trans))
+                except ValueError:
+                    print(i, line)
+                    return
+
+
+if __name__ == '__main__':
+    main()
