58bb1c729eb9d9a53894ae5a9d47babf90cd788e
Miyazawa Akira <pecorarista@gmail.com>
2024-06-03 11:35:09 +0900

    Update

--- gloss.py
+++ gloss.py
@@ -5,27 +5,28 @@ DELIM = '\t'
 
 def main() -> None:
     q = queue.Queue()
-    with (
-        Path('laplace.csv').open(mode='r') as r,
-        Path('gloss.tex').open(mode='w') as w
-    ):
-        for (i, line) in enumerate(r):
-            if len(line.strip()) == 0:
-                (fr, pr, en, trans) = zip(*q.queue)
-                s = f'''
+    gloss_dirname = Path('gloss')
+    for name in ['laplace', 'kolmogorov']:
+        input_filename = gloss_dirname.joinpath(f'{name}.csv')
+        output_filename = gloss_dirname.joinpath(f'{name}.tex')
+        with input_filename.open(mode='r') as r, output_filename.open(mode='w') as w:
+            for (i, line) in enumerate(r):
+                if len(line.strip()) == 0:
+                    (fr, pr, en, trans) = zip(*q.queue)
+                    s = f'''
     \\trigloss{{{' '.join(fr).strip()}}}
               {{{' '.join(pr).strip()}}}
               {{{' '.join(en).strip()}}}
               {{{' '.join(trans).strip()}}}\n'''
-                w.write(s)
-                q = queue.Queue()
-            else:
-                try:
-                    (fr, pr, en, trans) = line.replace('\n', '').split(DELIM)
-                    q.put((fr, pr, en, trans))
-                except ValueError:
-                    print(i, line)
-                    return
+                    w.write(s)
+                    q = queue.Queue()
+                else:
+                    try:
+                        (fr, pr, en, trans) = line.replace('\n', '').split(DELIM)
+                        q.put((fr, pr, en, trans))
+                    except ValueError:
+                        print(name, i, line)
+                        return
 
 
 if __name__ == '__main__':
